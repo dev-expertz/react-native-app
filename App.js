09f5aac976e9d8450c1d090baaa5c74a97745e56
Dev Expertz <dev.expertz@outlook.com>
2022-05-21 16:23:00 +0500

    Added dummy flexbox for concepts

--- App.js
+++ App.js
@@ -1,28 +1,75 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummyText}>Another piece of text!</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your Course Goal!"
+        ></TextInput>
+        <Button title="Add Goal" />
       </View>
-      <Text style={styles.dummyText}>Hello World!!</Text>
-      <Button title="Tap Me!" />
+      <View>
+        <Text>List of Goals...</Text>
+      </View>
+      {/* <View
+        style={{
+          height: 300,
+          flexDirection: "row",
+          justifyContent: "space-around",
+          alignItems: "stretch",
+          borderWidth: 1
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "red",
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>1</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "blue",
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>2</Text>
+        </View>
+        <View
+          style={{
+            backgroundColor: "green",
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text>3</Text>
+        </View>
+      </View> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 50,
   },
-  dummyText: {
-    margin: 16,
-    borderWidth: 2,
-    borderColor: "blue",
-    padding: 16,
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
   },
 });
