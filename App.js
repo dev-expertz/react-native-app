61a2eb217d5fbcd03dff9dd80b4b7a6f402309a3
Umair Ali <muhammad_umair_ali_malhi@live.com>
2022-05-21 15:30:49 +0500

    Added basic components

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.dummyText}>Another piece of text!</Text>
+      </View>
+      <Text style={styles.dummyText}>Hello World!!</Text>
+      <Button title="Tap Me!" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dummyText: {
+    margin: 16,
+    borderWidth: 2,
+    borderColor: "blue",
+    padding: 16,
+  },
+});
